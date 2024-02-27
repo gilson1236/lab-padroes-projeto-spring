13bbc3e1b74354695dcc72dc1a2b9413f4df91ff
gil <gilson1212@yahoo.com.br>
2024-02-27 17:20:01 -0300

    Project with swagger-ui

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/config/swagger/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.labpadroesprojetospring.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket clientApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("one.digitalinnovation.labpadroesprojetospring"))
+                .paths(PathSelectors.ant("/**"))
+                .build();
+    }
+}
